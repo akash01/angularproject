3a84ff53ed618a73d891407c9cef492ad9fe368a
mukesh <pace.akash@gmail.com>
2014-05-09 22:26:07 +0300

    added stylus support

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
-var express = require('express');
+var express = require('express'),
+	morgan = require('morgan'),
+	bodyParser = require('body-parser'),
+	stylus = require('stylus');
 
 //access development env , if not set , set it to development
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-//configuring the express app
-if ('development' == env ) {
-	app.set('views',__dirname+'/server/views');
-	app.set('view engine','jade');
+//configuring stylus
+function compile(str,path){
+	return stylus(str).set('filename',path);
 }
+
+app.use(express.static(__dirname+'/public'));
+app.use(morgan('dev'));
+app.use(bodyParser());
+app.use(stylus.middleware(
+	{ src: __dirname + '/public',
+		compile: compile
+	}
+));
+
+app.set('views',__dirname+'/server/views');
+app.set('view engine','jade');
+
+//configuring the express app
+// if ('development' == env ) {
+// app.set('views',__dirname+'/server/views');
+// app.set('view engine','jade');
+// }
 
 //adding the route, * all the request, js, css, with call back fn
 app.get('*', function(req,res){
