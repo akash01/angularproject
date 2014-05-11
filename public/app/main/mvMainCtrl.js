915663c3029bb5be6f46729e5198a2dd4500d818
mukesh <pace.akash@gmail.com>
2014-05-12 00:55:55 +0300

    caching implemented

--- public/app/main/mvMainCtrl.js
+++ public/app/main/mvMainCtrl.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('mvMainCtrl', function($scope,mvCourse) {
-  $scope.courses = mvCourse.query();
+angular.module('app').controller('mvMainCtrl', function($scope,mvCachedCourses) {
+  $scope.courses = mvCachedCourses.query();
 
 });
